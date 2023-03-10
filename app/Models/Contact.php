652e36e2ef1126b48e5f3e1a4eae53456689d76e
Joel Aduma <caesaragen@gmail.com>
2023-03-10 13:36:37 +0300

    add folders

---
+++ app/Models/Contact.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Contact extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'email',
+        'phone',
+    ];
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
+    }
+}
---
+++ app/Models/ContactUser.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ContactUser extends Model
+{
+    use HasFactory;
+
+    protected $table = 'contact_user';
+
+    protected $fillable = [
+        'user_id',
+        'contact_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function contact()
+    {
+        return $this->belongsTo(Contact::class);
+    }
+}
